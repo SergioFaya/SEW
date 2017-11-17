a040e41a357cab912bc84cc9e0d506c21aece7fd
Sergio <estisergio@gmail.com>
2017-11-17 19:52:41 +0100

    añadidos todos los campos del formulario faltan validar los blanks y enviar el email

--- p6/Ejercicio5/Encuesta.js
+++ p6/Ejercicio5/Encuesta.js
@@ -1,23 +1,82 @@
 
 class Encuesta{
+    //nombre apellidos sexo edad y email
     showDataForm(){
+        this.insertRow(0,"UO:","textUO","text");
+        this.insertRow(1,"Nombre:","textNombre","text");
+        this.insertRow(2,"Edad:","textUO","text");
+        this.insertRow(3,"email:","textEmail","text");
+        this.insertRowComboBoxGenre(4,"Sexo:");
+    }
+
+    insertRowComboBoxGenre(pos,textLabel){
         var table = document.getElementById("tableuser");
-        var row = table.insertRow(0);
+        var row = table.insertRow(pos);
         var cellLabel = row.insertCell(0);
-        var textNode = document.createTextNode("UO");
+        var textNode = document.createTextNode(textLabel);
+        cellLabel.appendChild(textNode);
+        var cellText = row.insertCell(1);
+        var select = document.createElement("select");
+        select.id = "cbSelectGenre";
+        var male = document.createElement("option");
+        male.text = "Masculino";
+        male.value = "Masculino";
+        var female = document.createElement("option");
+        female.text = "Femenino";
+        female.value = "Femenino";
+        var other = document.createElement("option");
+        other.text = "LGBTQIA";
+        other.value = "LGBTQIA";
+        var negros = document.createElement("option");
+        negros.text = "negros";
+        negros.value = "negros";
+        var moros = document.createElement("option");
+        moros.text = "moros";
+        moros.value = "moros";
+        var panchitos = document.createElement("option");
+        panchitos.text = "panchitos";
+        panchitos.value = "panchitos";
+        var rumanos = document.createElement("option");
+        rumanos.text = "rumanos";
+        rumanos.value = "rumanos";
+        select.appendChild(male);
+        select.appendChild(female);
+        select.appendChild(other);
+        select.appendChild(negros);
+        select.appendChild(moros);
+        select.appendChild(panchitos);
+        select.appendChild(rumanos);
+        cellText.appendChild(select);
+        
+        
+    }
+
+    insertRow(pos,textLabel,inputID,type){
+        var table = document.getElementById("tableuser");
+        var row = table.insertRow(pos);
+        var cellLabel = row.insertCell(0);
+        var textNode = document.createTextNode(textLabel);
         cellLabel.appendChild(textNode);
         var cellText = row.insertCell(1);
         var input = document.createElement("input");
-        input.type = "text";
-        input.name = "txtRow";
-        input.id = "txtRow";
+        input.type = type;
+        input.name = inputID;
+        input.id = inputID;
         input.size = 10;
         cellText.appendChild(input);
     }
 
     hideDataForm(){
         var table = document.getElementById("tableuser");
-        table.deleteRow(0);
+        var lenght = table.getElementsByTagName("tr").length;
+        while(lenght >0){
+            lenght--; 
+            table.deleteRow(lenght);
+        }   
+    }
+
+    sendMail(){
+        
     }
 
     onClick(){
