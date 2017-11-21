bb01f63ddff8f5008ad42c9504eff33eeea4d57c
unknown <Becario@USUARIO-K0PAFB4.aulasuo.uniovi.es>
2017-11-21 19:00:24 +0100

    tarea 1 session 7 casi acabada

---
+++ p7/ejercicio1/tarea1/Ejercicio1.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+    $("#showAll").click(function () {
+        $(":header:hidden,p:hidden,table:hidden").show();
+    });
+
+    $("#hideAll").click(function () {
+        $(":header:visible,p:visible,table:visible").hide();
+    });
+
+    $("#showHide").click(function () {
+        //Cambiar lo del combobox
+        var combo = document.getElementById("combobox");
+        var selectedItem = combo.options[combo.selectedIndex].value;
+        $("#" + selectedItem).toggle();
+        //$("#"+($(":option:selected").val()).toggle());
+    });
+
+    $("#addRows").click(function () {
+        var txt = $("#textRow").val();
+        $("#tabla tbody").append("<tr><td>"+txt+"</td></tr>");
+    });
+
+    $("#editH1").click(function(){
+        var txt = $("#textValueEditable").val();
+        $("#h1").text(txt);
+    });
+
+    $("#editH2").click(function(){
+        var txt = $("#textValueEditable").val();
+        $("#h2").text(txt);
+    });
+    $("#editH3").click(function(){
+        var txt = $("#textValueEditable").val();
+        $("#h3").text(txt);
+    }); 
+    $("#editP").click(function(){
+        var txt = $("#textValueEditable").val();
+        $("#p").text(txt);
+    });
+});
